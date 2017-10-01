90f993a1c51ac343ac95b64d56f17fe56564a6d6
김종현 <jhkim@dit.ac.kr>
2017-10-01 23:33:20 +0900

    plist load & pass to tableview

--- PlistToTableView/ViewController.swift
+++ PlistToTableView/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    var contents = NSArray()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        
+        // 데이터 로드
+        let path = Bundle.main.path(forResource: "Address2", ofType: "plist")
+        contents = NSArray(contentsOfFile: path!)!
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contents.count
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = myTableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        let myTitle = (contents[indexPath.row] as AnyObject).value(forKey: "title")
+        let myAddress = (contents[indexPath.row] as AnyObject).value(forKey: "address")
+        
+        print(myAddress!)
+        
+        myCell.textLabel?.text = myTitle as? String
+        myCell.detailTextLabel?.text = myAddress as? String
+        
+        return myCell
+    }
 
 }
-
